ef88ebc17709051f86bbae9b1e3b06ad22b8da9e
Kamil Szałkucki <ropeczek1997@gmail.com>
2018-10-16 15:20:50 +0200

    :D

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.open').click(function() {
+		$('#mySidenav').css('width', '250px');
+	});
+	
+	$('.closebtn').click(function() {
+		$('#mySidenav').css('width', '0');
+	});
+	
+ 
+});
