ed5227b5c11a22deee6b7cc43b58be4682ce762b
Kamil Szałkucki <ropeczek1997@gmail.com>
2018-10-16 18:29:19 +0200

    chybatak

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
+	if ($(window).width() > 960) {
+	$('#mySidenav').css('width', '250px');
+	$('main').css('marginLeft', '250px');
+	
 	$('.open').click(function() {
 		$('#mySidenav').css('width', '250px');
+		$('main').css('marginLeft', '250px');
 	});
 	
 	$('.closebtn').click(function() {
 		$('#mySidenav').css('width', '0');
+		$('main').css('marginLeft', '0');
 	});
-	
+	}else {
+		$('.open').click(function() {
+			$('#mySidenav').slideToggle('slow');
+			$('main').css('marginLeft', '0');
+		});
+	}
+		
  
 });
